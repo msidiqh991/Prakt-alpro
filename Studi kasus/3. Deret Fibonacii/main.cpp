624f102faa1edd80c79492cf300b1a873100795c
Muhammad Sidiq Hardiansyah <70674685+msidiqh991@users.noreply.github.com>
2022-04-13 07:04:27 +0000

    Studi Kasus Deret Fibonacci

---
+++ Studi kasus/3. Deret Fibonacii/main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Fibonacci{
+	public:
+		long deret(int);
+		long fibo(int);
+		
+	private:
+		int n;
+		
+};
+
+long Fibonacci::deret(int n) {
+  long fib_sub, f1=0, f2=1;
+  cout << "- Rekursif  -" <<endl;
+  cout << "Nilai Fibonacci : ";
+     for(int i=0; i<n; i++){
+        fib_sub = f1 + f2;
+        f2 = f1;
+        f1 = fib_sub;
+        cout << fib_sub <<" ";
+    }
+}
+
+long Fibonacci::fibo(int n) {
+	if(n==0 || n==1) {
+		return (1);	
+	} else {
+    	return (fibo(n-1) + fibo(n-2));
+  }
+}
+
+int main(){
+	int p;
+	long hasil;
+	Fibonacci x;
+	cout << "Masukkan Bilangan : ";
+	cin >> p;
+	x.deret(p);
+	cout << endl <<endl;
+	cout << "- Iteratif -" <<endl ;
+	cout  << "Nilai Fibonacci : ";
+	for(int i=0; i<p; i++) {
+		cout << x.fibo(i) <<" ";
+	}
+ 	return 0;
+}
